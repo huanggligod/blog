04e2482b832cbbcbe19236adc860d89080dcc10b
huanggang <2293039558@qq.com>
2020-09-02 17:40:01 +0800

    缓存

---
+++ demo/calc/index.js
@@ -0,0 +1,14 @@
+(function(){
+   "use strict";
+   
+    // 获取元素
+    var el = function(element) {
+        if (element.chartAt(0) === '#') {
+            return document.querySelector(element)
+        }
+
+        return document.querySelector
+    }
+
+    
+}())
